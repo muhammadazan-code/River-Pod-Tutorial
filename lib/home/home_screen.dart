8cd2ba1d9f16410326c60ad0e03fbf720f5b2d4a
muhammadazan-code <azanjokhio54@gmail.com>
2026-03-12 07:11:39 +0500

    State Notifier

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,30 +1,32 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_tutorial/search_provider.dart';
 
 // To read constant value.
-final name = Provider<String>((ref) => "Azan");
-final age = Provider<int>((ref) => 20);
-final country = Provider<String>((ref) => "Pakistan");
+// final name = Provider<String>((ref) => "Azan");
+// final age = Provider<int>((ref) => 20);
+// final country = Provider<String>((ref) => "Pakistan");
 
-class HomeScreen extends ConsumerStatefulWidget {
-  const HomeScreen({super.key});
+// class HomeScreen extends ConsumerStatefulWidget {
+//   const HomeScreen({super.key});
 
-  @override
-  ConsumerState<HomeScreen> createState() => _HomeScreenState();
-}
+//   @override
+//   ConsumerState<HomeScreen> createState() => _HomeScreenState();
+// }
 
-class _HomeScreenState extends ConsumerState<HomeScreen> {
-  @override
-  Widget build(BuildContext context) {
-    final a = ref.watch(name);
-    final b = ref.watch(age);
-    final c = ref.watch(country);
-    return Scaffold(
-      appBar: AppBar(title: Center(child: Text(a))),
-      body: ListTile(title: Text(a), subtitle: Text("$b\n$c")),
-    );
-  }
-}
+// class _HomeScreenState extends ConsumerState<HomeScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     final a = ref.watch(name);
+//     final b = ref.watch(age);
+//     final c = ref.watch(country);
+//     return Scaffold(
+//       appBar: AppBar(title: Center(child: Text(a))),
+//       body: ListTile(title: Text(a), subtitle: Text("$b\n$c")),
+//     );
+//   }
+// }
 
 // class HomeScreen extends ConsumerWidget {
 //   const HomeScreen({super.key});
@@ -40,3 +42,53 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
 //     );
 //   }
 // }
+class HomeScreen extends ConsumerWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    if (kDebugMode) {
+      print("object");
+    }
+    return Scaffold(
+      appBar: AppBar(title: Center(child: Text("Search Provider"))),
+      body: Column(
+        children: [
+          TextFormField(
+            onChanged: (value) {
+              ref.read(searchProvider.notifier).search(value);
+            },
+          ),
+          Consumer(
+            builder: (context, ref, child) {
+              if (kDebugMode) {
+                print("Text Form Field");
+              }
+              final search = ref.watch(
+                searchProvider.select((state) => state.search),
+              );
+              return Text(search);
+            },
+          ),
+          Divider(),
+          Consumer(
+            builder: (context, ref, child) {
+              if (kDebugMode) {
+                print("Switch");
+              }
+              final search = ref.watch(
+                searchProvider.select((state) => state.isChanged),
+              );
+              return Switch(
+                value: search,
+                onChanged: (value) {
+                  ref.read(searchProvider.notifier).onChange(value);
+                },
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
